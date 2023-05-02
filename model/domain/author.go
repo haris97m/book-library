7f539a952b1e16a33e02ae6b6566a0aa7aaddcde
haris97m <haris00mubaroq@gmail.com>
2023-05-02 15:13:07 +0700

    fix DOD updated with null value

--- model/domain/author.go
+++ model/domain/author.go
@@ -8,12 +8,12 @@ import (
 
 // STRUCTURE TABLE
 type Author struct {
-	ID          uint      `gorm:"primaryKey"`
-	FirstName   string    `gorm:"size:100;not null"`
-	LastName    string    `gorm:"size:100;not null"`
-	DateOfBirth time.Time `gorm:"type:date;not null"`
-	DateOfDeath time.Time `gorm:"type:date;null"`
-	Books       []Book    `gorm:"foreignKey:AuthorID"`
+	ID          uint       `gorm:"primaryKey"`
+	FirstName   string     `gorm:"size:100;not null"`
+	LastName    string     `gorm:"size:100;not null"`
+	DateOfBirth time.Time  `gorm:"type:date;not null"`
+	DateOfDeath *time.Time `gorm:"type:date;default:null"`
+	Books       []Book     `gorm:"foreignKey:AuthorID"`
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 	DeletedAt   gorm.DeletedAt `gorm:"index"`
--- model/web/author.go
+++ model/web/author.go
@@ -1,30 +1,49 @@
 package web
 
 import (
+	"encoding/json"
 	"time"
 )
 
+type myTime time.Time
+
+var _ json.Unmarshaler = &myTime{}
+
+func (mt *myTime) UnmarshalJSON(bs []byte) error {
+	var s string
+	err := json.Unmarshal(bs, &s)
+	if err != nil {
+		return err
+	}
+	t, err := time.ParseInLocation("2006-01-02", s, time.UTC)
+	if err != nil {
+		return err
+	}
+	*mt = myTime(t)
+	return nil
+}
+
 type AuthorCreateRequest struct {
-	FirstName   string    `json:"first_name" validate:"required"`
-	LastName    string    `json:"last_name" validate:"required"`
-	DateOfBirth time.Time `json:"date_of_birth" validate:"required"`
-	DateOfDeath time.Time `json:"date_of_death" validate:"default=null"`
+	FirstName   string     `json:"first_name" validate:"required"`
+	LastName    string     `json:"last_name" validate:"required"`
+	DateOfBirth time.Time  `json:"date_of_birth" validate:"required"`
+	DateOfDeath *time.Time `json:"date_of_death" default:"null"`
 }
 
 type AuthorUpdateRequest struct {
-	ID          int       `json:"id"`
-	FirstName   string    `json:"first_name" validate:"required"`
-	LastName    string    `json:"last_name" validate:"required"`
-	DateOfBirth time.Time `json:"date_of_birth" validate:"required"`
-	DateOfDeath time.Time `json:"date_of_death" validate:"default=null"`
+	ID          int        `json:"id"`
+	FirstName   string     `json:"first_name" validate:"required"`
+	LastName    string     `json:"last_name" validate:"required"`
+	DateOfBirth time.Time  `json:"date_of_birth" validate:"required"`
+	DateOfDeath *time.Time `json:"date_of_death" default:"null"`
 }
 
 type AuthorResponse struct {
-	ID          uint      `json:"id"`
-	FirstName   string    `json:"first_name"`
-	LastName    string    `json:"last_name"`
-	DateOfBirth time.Time `json:"date_of_birth"`
-	DateOfDeath time.Time `json:"date_of_death"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID          uint       `json:"id"`
+	FirstName   string     `json:"first_name"`
+	LastName    string     `json:"last_name"`
+	DateOfBirth time.Time  `json:"date_of_birth"`
+	DateOfDeath *time.Time `json:"date_of_death"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
 }
